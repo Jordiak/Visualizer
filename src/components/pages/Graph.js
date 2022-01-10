8215b166b1c3da01b795b5b3c05e3b4e00d60f4b
Staniell <giostaniell12@gmail.com>
2022-01-10 19:29:18 +0800

    test

--- src/components/pages/Graph.js
+++ src/components/pages/Graph.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Graph(){
     return(
         <>
-            <h1 className='Graph'>GRAPH</h1>
+            <h1 className='Graph'>Test GRAPH</h1>
         </>
     );
 }
