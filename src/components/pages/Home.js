7b6f11222df514cae221d5f1007c7cc13def6df4
Staniell <giostaniell12@gmail.com>
2022-01-11 19:49:56 +0800

    button for generating random numbers

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 
 const Home = () => {
+    const handleClick = () =>{
+        var array = Array.from({length: 10}, () => Math.floor(Math.random() * 50))
+        console.log(array.join(",").toString());
+    }
+
     return(
         <div className='Home'>
             {/* <h1>HOME</h1> */}
             <label htmlFor='quantity'>Number of Items :</label>
             <input type={Int32Array} id='quantity' name='quantity' min={1} max={10}></input>
-            <button className='btn'>Button</button>
+            <button onClick={handleClick} className='btn'>Button</button>
         </div>
     );
 }
