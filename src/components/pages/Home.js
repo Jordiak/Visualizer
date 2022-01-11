ecdeb0f1ee086f02235e2a8a60fb2df22826c262
Staniell <giostaniell12@gmail.com>
2022-01-11 22:35:42 +0800

    added results to a textbox

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { useState } from 'react';
 
 const Home = () => {
+    const [ran_numbers, setData] = useState("")
+
     const handleClick = (value) =>{
-        var array = Array.from({length: value}, () => Math.floor(Math.random() * 50))
-        console.log(array.join(",").toString());
+        if(Number.isNaN(value))
+            setData("Enter numerical values only")
+        else{
+            var array = Array.from({length: value}, () => Math.floor(Math.random() * 50))
+            var result = array.join(",").toString();
+            if(value == "")
+                setData("Enter the number of random numbers to be generated")
+            else
+                setData(result)
+        }
     }
 
     return(
@@ -12,7 +22,8 @@ const Home = () => {
             {/* <h1>HOME</h1> */}
             <label htmlFor='quantity'>Number of Items :</label>
             <input type={Int32Array} id='quantity' name='quantity' min={1} max={10}></input>
-            <button onClick={() =>handleClick(parseInt(document.getElementById("quantity").value))} className='btn'>Button</button>
+            <button onClick={() =>handleClick(parseInt(document.getElementById("quantity").value))} className='btn'>Enter</button>
+             <label>Results:</label><input type="text" value={ran_numbers}></input>
         </div>
     );
 }
