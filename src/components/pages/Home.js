8c15cbf6caedab4609c2cd9bc80a945a0e291363
Staniell <giostaniell12@gmail.com>
2022-01-11 19:29:04 +0800

    x

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export default function Home(){
+
+const Home = () => {
     return(
         <div className='Home'>
             {/* <h1>HOME</h1> */}
-
-            <label htmlFor='quantity'>Quantity :</label>
+            <label htmlFor='quantity'>Number of Items :</label>
             <input type={Int32Array} id='quantity' name='quantity' min={1} max={10}></input>
+            <button className='btn'>Button</button>
         </div>
     );
 }
+
+export default Home;
