2c55ec8d9d2d5549e54594c18e27f37f37acc464
Staniell <giostaniell12@gmail.com>
2022-01-12 17:37:07 +0800

    able to pass val from child comp to parent comp

--- src/components/functions/Ran_num_input.js
+++ src/components/functions/Ran_num_input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Ran_nums = () => {
+const Ran_nums = ({setCount}) => {
     const [ran_numbers, setData] = useState("")
 
     const handleClick = (value) =>{
@@ -14,6 +14,7 @@ const Ran_nums = () => {
                 setData("Enter the number of random numbers to be generated")
             else
                 setData(result)
+            setCount(result)
         }
     }
 
--- src/components/pages/Stack.js
+++ src/components/pages/Stack.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Ran_nums from '../functions/Ran_num_input';
+import { useState } from 'react';
 
 export default function Stack(){
+    const [random_numbers, setCount] = useState();
     return(
         <>
         <center><h1>STACK</h1></center>
-        <Ran_nums/>
+        <Ran_nums setCount={setCount}/>
+        {random_numbers}
+
         </>
     );
 }
