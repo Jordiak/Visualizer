8034107d62379080d81240c719e7cccc743f520f
Staniell <giostaniell12@gmail.com>
2022-01-12 17:00:07 +0800

    adjusted comment

--- src/components/pages/Stack.js
+++ src/components/pages/Stack.js
@@ -6,8 +6,6 @@ export default function Stack(){
         <>
         <center><h1>STACK</h1></center>
         <Ran_nums/>
-
-        I want to get the the value from the txtbox1 that came from the child component Ran_nums
         </>
     );
 }
