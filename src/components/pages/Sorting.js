296d8b9b9c5a4a4b23aa55a6feffef67de988212
Staniell <giostaniell12@gmail.com>
2022-01-21 21:49:43 +0800

    added insertion sort

--- src/components/pages/Sorting.js
+++ src/components/pages/Sorting.js
@@ -84,127 +84,40 @@ export default function Sorting(){
         </a>
       </p>
             </TabPanel>
+            <TabPanel style={{padding:"10px"}}>
+                    def insertionSort(alist):<br></br>
+for i in range(1,len(alist)):<br></br>
+
+#el current = alist[i]<br></br>
+
+    #comparing the current element with the sorted portion and swapping<br></br>
+    while i &gt; 0 and alist[i-1]&gt;current:<br></br>
+        alist[i] = alist[i-1]<br></br>
+        i = i-1<br></br>
+       alist[i] = current<br></br>
+
+    #print(alist)<br></br>
+
+return alist
+ement to be compared<br></br>
+<p>
+        Source:{' '}
+        <a href="https://www.programiz.com/dsa/insertion-sort" target="_blank">
+        programiz.com/dsa/insertion-sort
+        </a>
+      </p>
+   
+            </TabPanel>
+            <TabPanel>
+
+            </TabPanel>
             <TabPanel>
                 <h2>
-                    {/* <p>def insertionSort(alist):
 
-for i in range(1,len(alist)):
-
-    #element to be compared
-    current = alist[i]
-
-    #comparing the current element with the sorted portion and swapping
-    while i>0 and alist[i-1]>current:
-        alist[i] = alist[i-1]
-        i = i-1
-       alist[i] = current
-
-    #print(alist)
-
-return alist</p> */}
 </h2>
             </TabPanel>
             <TabPanel>
-                <h2>
-                    {/* <p>def merge_sort(arr):
-    # The last array split
-    if len(arr) <= 1:
-        return arr
-    mid = len(arr) // 2
-    # Perform merge_sort recursively on both halves
-    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
-
-    # Merge each side together
-    return merge(left, right, arr.copy())
-
-
-def merge(left, right, merged):
-
-    left_cursor, right_cursor = 0, 0
-    while left_cursor < len(left) and right_cursor < len(right):
-      
-        # Sort each one and place into the result
-        if left[left_cursor] <= right[right_cursor]:
-            merged[left_cursor+right_cursor]=left[left_cursor]
-            left_cursor += 1
-        else:
-            merged[left_cursor + right_cursor] = right[right_cursor]
-            right_cursor += 1
-            
-    for left_cursor in range(left_cursor, len(left)):
-        merged[left_cursor + right_cursor] = left[left_cursor]
-        
-    for right_cursor in range(right_cursor, len(right)):
-        merged[left_cursor + right_cursor] = right[right_cursor]
-
-    return merged
-    </p> */}
-    </h2>
-            </TabPanel>
-            <TabPanel>
-                <h2>
-                {/* def partition(array, begin, end):
-    pivot_idx = begin
-    for i in xrange(begin+1, end+1):
-        if array[i] <= array[begin]:
-            pivot_idx += 1
-            array[i], array[pivot_idx] = array[pivot_idx], array[i]
-    array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
-    return pivot_idx
-
-def quick_sort_recursion(array, begin, end):
-    if begin >= end:
-        return
-    pivot_idx = partition(array, begin, end)
-    quick_sort_recursion(array, begin, pivot_idx-1)
-    quick_sort_recursion(array, pivot_idx+1, end)
-
-def quick_sort(array, begin=0, end=None):
-    if end is None:
-        end = len(array) - 1
-    
-    return quick_sort_recursion(array, begin, end) */}
-</h2>
-            </TabPanel>
-            <TabPanel>
-                <h2>  {/*# To heapify subtree rooted at index i. 
-# n is size of heap 
-def heapify(arr, n, i): 
-	largest = i # Initialize largest as root 
-	l = 2 * i + 1	 # left = 2*i + 1 
-	r = 2 * i + 2	 # right = 2*i + 2 
-
-	# See if left child of root exists and is 
-	# greater than root 
-	if l < n and arr[i] < arr[l]: 
-		largest = l 
-
-	# See if right child of root exists and is 
-	# greater than root 
-	if r < n and arr[largest] < arr[r]: 
-		largest = r 
-
-	# Change root, if needed 
-	if largest != i: 
-		arr[i],arr[largest] = arr[largest],arr[i] # swap 
-
-		# Heapify the root. 
-		heapify(arr, n, largest) 
-
-# The main function to sort an array of given size 
-def heapSort(arr): 
-	n = len(arr) 
-
-	# Build a maxheap. 
-	for i in range(n, -1, -1): 
-		heapify(arr, n, i) 
-
-	# One by one extract elements 
-	for i in range(n-1, 0, -1): 
-		arr[i], arr[0] = arr[0], arr[i] # swap 
-		heapify(arr, i, 0) 
-
-heapSort(arr)  */}</h2>
+                <h2>  </h2>
             </TabPanel>
         </Tabs>
             </div>
