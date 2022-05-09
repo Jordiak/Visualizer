e61751336a477900534927cfc93c9fe3128be78b
Jordiak <jeheath@live.mcl.edu.ph>
2022-05-09 16:10:43 +0800

    Changed UI for Score Page and added FormatMonth and FormatTime function files

--- client/src/components/pages/ScorePage.js
+++ client/src/components/pages/ScorePage.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
 import Axios from 'axios';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import FormatMonth from '../functions/formatMonth';
+import FormatTime from '../functions/formatTime';
 
 export default function ScorePage(){
 
@@ -13,7 +17,36 @@ export default function ScorePage(){
       });
     } , [])
 
-    
+    //function for calculating percentages of the scores
+    function calculatePercent (score, total){
+        return ((parseInt(score) / parseInt(total)) * 100).toFixed(2);
+    }
+
+    //function for calculating the color value of the trails in the progress bars
+    function calculateColor (percent){
+        if (percent >= 0 && percent <= 25)
+            return "#C0392B"
+        else if (percent >= 26 && percent <= 50)
+            return "#F39C12"
+        else if (percent >= 51 && percent <= 75)
+            return "#F1C40F"
+        else if (percent >= 76 && percent <= 100)
+            return "##2ECC71"
+    }
+
+    //function for formating datetime string
+    function formatDateTime(dateTimeString){
+        let year = dateTimeString.slice(0,4);
+        let month = dateTimeString.slice(5,7)
+        let day = dateTimeString.slice(8,10)
+        let hour = dateTimeString.slice(11,13)
+        let minute = dateTimeString.slice(14,16)
+
+        let formatedMonth = FormatMonth(month)
+        let formatedTime = FormatTime(hour,minute)
+        
+        return formatedMonth + " " + day + ", " + year + " - " + formatedTime
+    }
     
     return(
         <div className="InformationBox1">
@@ -21,14 +54,44 @@ export default function ScorePage(){
             {
                 scoreList.map((item)=>{
                     return (
-                     <div className="card">
-                        {item.useremail_reg}
-                        <br/>
-                        {item.user_score}
-                        <br/>
-                        {item.questions_total}
-                        <br/>
-                        {item.quiz_taken}
+                     <div className="score-card">
+                        <div className = "score-information-holder">
+                            <h1 >{item.useremail_reg}</h1>
+                            Score for daily quiz:
+                            <br/>
+                            {item.user_score} / {item.questions_total}
+                            <br/>
+                            <br/>
+                            Taken on:
+                            <br/>
+                            {formatDateTime(item.quiz_taken)}
+                        </div>
+                        <div className="progress-circle-container">
+                            <CircularProgressbar
+                                value={parseInt(item.user_score)}
+                                maxValue={parseInt(item.questions_total)}
+                                text={`${calculatePercent(item.user_score,item.questions_total)} %`}
+                                circleRatio={0.7} //Makes a semi-circle that is 70% of a full circle.
+                                styles={{
+                                    trail:{
+                                        strokeLinecap: "butt",
+                                        transform: "rotate(-126deg)",
+                                        transformOrigin: "center center",
+                                    },
+                                    path:{
+                                        strokeLinecap: "butt",
+                                        transform: "rotate(-126deg)",
+                                        transformOrigin: "center center",
+                                        stroke: calculateColor(calculatePercent(item.user_score,item.questions_total)), 
+                                    },
+                                    text:{
+                                        fill: calculateColor(calculatePercent(item.user_score,item.questions_total))
+                                    }
+                                }}
+                                strokeWidth = {10}
+                            >
+                            </CircularProgressbar>
+                        </div>
                      </div>
                 )})
             }
